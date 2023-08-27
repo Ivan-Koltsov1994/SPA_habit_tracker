056b5a3435b86f0da26141d8158ae5bd3699984a
Ivan <ivan.koltsov.1994@mail.com>
2023-08-27 15:21:55 +0500

    add admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email','place', 'phone', 'is_active',)
+    list_filter = ('id','is_active',)
+
