bdd7f3e5df31cfa9b23e7199223c9edc1627f102
Ivan <ivan.koltsov.1994@mail.com>
2023-08-27 15:09:11 +0500

    add HabitViewSet;

--- habit/views.py
+++ habit/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from habit.models import Habit
+from habit.serializers import HabitSerializers
+
 
 # Create your views here.
+class HabitViewSet(viewsets.ModelViewSet):
+    serializer_class = HabitSerializers
+    queryset = Habit.objects.all()
