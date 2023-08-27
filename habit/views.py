35faa0b80af35ebb9ef578e3852220337eb4fd0f
Ivan <ivan.koltsov.1994@mail.com>
2023-08-27 15:17:19 +0500

    correct view.py

--- habit/views.py
+++ habit/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 
 from habit.models import Habit
 from habit.serializers import HabitSerializers
-
+from users.models import UserRoles
 
 # Create your views here.
 class HabitViewSet(viewsets.ModelViewSet):
+    """Вывод списка всех привычек"""
     serializer_class = HabitSerializers
     queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Фильтруем список привычек только для владельца или модератора"""
+        user = self.request.user
+        if user.is_staff or user.is_superuser or user.role == UserRoles.MODERATOR:
+            return Habit.objects.all()
+        else:
+            return Habit.objects.filter(owner=user)
+
+class HabitsListView(generics.ListAPIView):
+    """Вывод списка  публичных привычек"""
+    serializer_class = HabitSerializers
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+    def get_queryset(self):
+        """Фильтруем список привычек только для владельца или модератора"""
+        user = self.request.user
+        if user.is_staff or user.is_superuser or user.role == UserRoles.MODERATOR:
+            return Habit.objects.all()
+        else:
+            return Habit.objects.filter(public=True)
