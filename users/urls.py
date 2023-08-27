d49862adbb174b72230cf7c5509df22b17c5eee2
Ivan <ivan.koltsov.1994@mail.com>
2023-08-27 14:05:08 +0500

    add users serialazers

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+
+urlpatterns = [
+    path('users/', UsersListView.as_view(), name='users_list'),
+    path('users/<int:pk>/', UsersDetailView.as_view(), name='users_detail'),
+    path('user/create/', UsersCreateView.as_view(), name='user_create'),
+    path('user/<int:pk>/update/', UsersUpdateView.as_view(), name='user_update'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+              ]
