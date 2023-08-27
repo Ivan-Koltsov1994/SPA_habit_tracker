1f81cf7b4162410bf868954f44448313733ae81c
Ivan <ivan.koltsov.1994@mail.com>
2023-08-27 13:56:59 +0500

    add model user

--- users/models.py
+++ users/models.py
@@ -1,6 +1,74 @@
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import UserManager as UserBaseManager
 from django.db import models
-
+from django.apps import apps
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+NULLABLE = {'null': True, 'blank': True}
+
+class UserCManager(UserBaseManager):
+    """переопределение модели для команды python manage.py createsuperuser (для поля email)"""
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+
+        if not email:
+            raise ValueError("The given username must be set")
+        email = self.normalize_email(email)
+        GlobalUserModel = apps.get_model(
+            self.model._meta.app_label, self.model._meta.object_name
+        )
+        email = GlobalUserModel.normalize_username(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+class UserRoles(models.TextChoices):
+    MEMBER = 'member', _('member')
+    MODERATOR = 'moderator', _('moderator')
+
 class User(AbstractUser):
-    pass
+    """Модель пользователя"""
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    city = models.CharField(max_length=100, verbose_name='Город', **NULLABLE)
+    role = models.CharField(max_length=10, choices=UserRoles.choices, default=UserRoles.MEMBER, verbose_name='роль',
+                            **NULLABLE)
+    # Установка флага активности пользователя
+    is_active = models.BooleanField(default=True, verbose_name='активность')
+    chat_id = models.CharField(max_length=15, verbose_name='ID бота TG', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserCManager()
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        unique_together = ('email', 'phone')
+        ordering = ('email',)  # сортировка, '-email' - сортировка в обратном порядке
+
+    def __str__(self):
+        return f'{self.email} - {self.phone}: {self.city}'
